edaba6a91f524d038ddcb4dac9580ec375e12db1
Pranta-Das <prantodas423@gmail.com>
2025-04-14 19:44:55 +0600

    for..in

--- objectSeal.js
+++ objectSeal.js
@@ -4,6 +4,9 @@ const profile = {
     religion: 'hinduism',
     address: 'chittagong, Bangladesh'
 }
+for(const propertys in profile){
+    console.log(propertys);
+}
 Object.seal(profile);
 //if you use Object.freeze then you can't delete or add  any object property.but you can update.
 profile.age = 9;
