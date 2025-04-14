6b440d2f92a34d578c4b71cf6ed8e2e3475c7284
Pranta-Das <prantodas423@gmail.com>
2025-04-14 19:35:48 +0600

    message

--- objectFreeze.js
+++ objectFreeze.js
@@ -5,5 +5,6 @@ const profile = {
     address: 'chittagong, Bangladesh'
 }
 Object.freeze(profile);
+//if you use Object.freeze then you can't delete or add or update any object property.
 delete profile.age;
 console.log(profile);
