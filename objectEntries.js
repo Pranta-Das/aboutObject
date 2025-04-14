5824add0dfe6641005f7025df0c585dad8e31579
Pranta-Das <prantodas423@gmail.com>
2025-04-14 19:19:26 +0600

    Object.entries

---
+++ objectEntries.js
@@ -0,0 +1,11 @@
+const profile = {
+    name:'joy',
+    age:24,
+    religion: 'hinduism',
+    address: 'chittagong, Bangladesh'
+}
+const entries = Object.entries(profile);
+console.log(entries);
+// for(const entri of entries){
+//     console.log(entri);
+// }
